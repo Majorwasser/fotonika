7d7c5416ed05ab702608b88ad7ffb0250d8773a6
Majorwasser <patrykk.wodecki@gmail.com>
2026-05-24 23:09:05 +0200

    Dzien 4: Stworzenie klas pod budowe drzewka AST

--- parser.py
+++ parser.py
@@ -6,3 +6,23 @@ plus.append(parser)
 
 print(plus)
 
+from dataclasses import dataclass
+@dataclass
+class Number:
+    value:float
+@dataclass
+class BinaryOp:
+    left:str
+    op:str
+    right:str
+left = Number(3)
+op = "*"
+right = Number(4)
+
+t = BinaryOp(
+left = left,
+op = op,
+right = right,
+)
+
+print(t)
