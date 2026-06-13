7514a4783f1fa980dd302f2cc6ce0cb22ea78493
Majorwasser <patrykk.wodecki@gmail.com>
2026-06-13 23:54:38 +0200

    Day21 praca dalej nad emulatorem

--- emulator.py
+++ emulator.py
@@ -6,6 +6,15 @@ stos =[]
 def czytanielinijek (linijki):
     for yalla in linijki:
         if yalla.startswith("LOAD"):
-            stos.append(yalla[4:])
+            stos.append(float(yalla[4:]))
+        if yalla.startswith("MUL"):
+            b = stos.pop()
+            a = stos.pop()
+            stos.append(float(a) * float(b))
+        if yalla.startswith("ADD"):
+            b = stos.pop()
+            a = stos.pop()
+            stos.append(float(a) + float(b))
+        return stos
             
-    
+print (czytanielinijek(linijki))
